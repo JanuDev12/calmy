9f5df0a21b426660ef5a01baa4719742fe44dfd7
JanuDev <darkober12@gmail.com>
2025-02-25 19:32:35 -0400

    feat: add breathing circle

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,84 @@
 import './App.css'
 import { motion } from "motion/react";
 import Breathcircle from './components/Breathcircle';
+import { useEffect, useMemo, useState } from 'react';
+
+// Define the phases of breathing
+const PHASES = [
+  {
+    name: "inhale",
+    duration: 4000,
+    transform: "scale(1.5)",
+  },
+  {
+    name: "inhaleHold",
+    duration: 4000,
+    transform: "scale(1.5)",
+  },
+  {
+    name: "exhale",
+    duration: 4000,
+    transform: "scale(1)",
+  },
+  {
+    name: "exhaleHold",
+    duration: 4000,
+    transform: "scale(1)",
+  },
+];
+
 
 function App() {
 
+  // Initial Phase on inhale
+  const [phase, setPhase] = useState(PHASES[0].name);
+
+
+  // Creating animations for each phase
+  const animationVariants = useMemo(() => {
+    return PHASES.reduce((variants, config) => {
+      variants[config.name] = {
+        transform: config.transform,
+        // Motion expects seconds
+        transition: { duration: config.duration / 1000, ease: "lineal"},
+      };
+      return variants;
+    }, {});
+  }, []);
+
+
+  // useEffect for transitioning to each phase of breathing
+  useEffect(() => {
+    const currentPhase = PHASES.findIndex(
+      (config) => config.name === phase
+    );
+    const nextPhase = (currentPhase + 1) % PHASES.length;
+    // Timer for calculate when transitioning to nextPhase
+    const timer = setTimeout(() => {
+      setPhase(PHASES[nextPhase].name);
+    }, PHASES[currentPhase].duration);
+    return () => clearTimeout(timer);
+  }, [phase]);
+
   return (
     <>
-      <div className="w-full h-screen bg-amber-100 flex justify-center items-center">
-        <motion.div className="w-2xl h-1/2 border-2 border-red-300 flex items-center">
-          <Breathcircle/>
+      <div className="w-full h-screen bg-amber-100 flex flex-col justify-center items-center">
+        <motion.div
+          className="w-2xl h-1/2 border-2 border-red-300 flex items-center"
+        >
+          <motion.div
+            className="mx-auto size-32 rounded-full border-2 bg-blue-300"
+            variants={animationVariants}
+            animate={phase}
+            initial={{transform: "scale(1)" }}
+            transition={{ease: 'linear'}}
+          ></motion.div>
         </motion.div>
+        <div className="w-xl mt-20 flex justify-between">
+          <button className="cursor-pointer">Start</button>
+          <button className="cursor-pointer">Pause</button>
+          <span>Fase actual de la respiracion: {phase}</span>
+        </div>
       </div>
     </>
   );
--- src/components/BreathCircle.tsx
+++ src/components/BreathCircle.tsx
@@ -1,16 +1,31 @@
-import { motion } from "motion/react";
+import { motion, useAnimate } from "motion/react";
+import { useEffect } from "react";
 
 
-function Breathcircle() {
+const Breathcircle =  ({
+    inhaleDuration = 4, 
+    holdDuration = 7,
+    exhaleDuration = 8,
+    initialScale = 1,
+    inhaleScale = 1.5,
+    size = 150
+}) => {
+ 
+    const totalDuration = inhaleDuration + holdDuration + exhaleDuration;
+
+    const times = [0, inhaleDuration / totalDuration, (inhaleDuration + holdDuration) / totalDuration, 1, ]
+
   return (
-      <motion.div
-        className="mx-auto size-32 rounded-full border-2 border-blue-300"
-        animate={{
-          scale: 1.5,
-          transition: { duration: 2, repeat: Infinity },
-        }}
-      ></motion.div>
-  
+    <motion.div
+      className="mx-auto size-32 rounded-full border-2 bg-blue-300"
+      animate={{ scale: [initialScale, inhaleScale, inhaleScale, initialScale]}}
+      transition={{
+        duration: totalDuration,
+        times,
+        repeat: Infinity
+      }}
+      style={{width: size, height: size}}
+    ></motion.div>
   );
 }
 

dd9e82897b50006d1138c22e153bd0037502b45c
JanuDev <darkober12@gmail.com>
2025-02-28 07:20:54 -0400

    feat: defining ui

---
+++ src/components/IconVolumeOff.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { VolumeOff } from "lucide-react";
+
+
+function IconVolumeOff() {
+  return (
+    <VolumeOff size={18}></VolumeOff>
+  )
+}
+
+export default IconVolumeOff
